8a315c8258ff7d2fe7bbfdb7552b83bd62b110a9
Khalid <khalidtheeb@gmail.com>
2018-06-14 00:33:21 +0300

    Update sparse_conversions.h

--- ell_SPMM/sparse_conversions.h
+++ ell_SPMM/sparse_conversions.h
@@ -188,6 +188,7 @@ coo_matrix<IndexType, ValueType>
     return coo;
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////
 //! Convert COO format to CSR format
 // Storage for output is assumed to have been allocated
