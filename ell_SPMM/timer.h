c6396a6f32a8e227494ff40d335b9ec8cd97b9ab
Khalid <khalidtheeb@gmail.com>
2018-06-14 00:22:21 +0300

    Update timer.h

--- ell_SPMM/timer.h
+++ ell_SPMM/timer.h
@@ -14,46 +14,44 @@
  *  limitations under the License.
  */
 
-
-
 #pragma once
 
 // A simple timer class
 
-#include "mem.h"
-#include "cuda.h"
+#include <cuda.h>
 
 class timer
 {
     cudaEvent_t start;
     cudaEvent_t end;
 
-    public:
+public:
     timer()
-    { 
-        CUDA_SAFE_CALL(cudaEventCreate(&start)); 
-        CUDA_SAFE_CALL(cudaEventCreate(&end));
-        CUDA_SAFE_CALL(cudaEventRecord(start,0));
+    {
+        cudaEventCreate(&start);
+        cudaEventCreate(&end);
+        cudaEventRecord(start,0);
     }
 
     ~timer()
     {
-        CUDA_SAFE_CALL(cudaEventDestroy(start));
-        CUDA_SAFE_CALL(cudaEventDestroy(end));
+        cudaEventDestroy(start);
+        cudaEventDestroy(end);
     }
 
     float milliseconds_elapsed()
-    { 
+    {
         float elapsed_time;
-        CUDA_SAFE_CALL(cudaEventRecord(end, 0));
-        CUDA_SAFE_CALL(cudaEventSynchronize(end));
-        CUDA_SAFE_CALL(cudaEventElapsedTime(&elapsed_time, start, end));
+        cudaEventRecord(end, 0);
+        cudaEventSynchronize(end);
+        cudaEventElapsedTime(&elapsed_time, start, end);
         return elapsed_time;
     }
     float seconds_elapsed()
-    { 
+    {
         return milliseconds_elapsed() / 1000.0;
     }
 };
+
 
 
