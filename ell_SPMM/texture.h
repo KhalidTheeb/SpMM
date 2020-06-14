d47ca6c373a4a7d1db3204f9ce21298d13d4c778
Khalid <khalidtheeb@gmail.com>
2020-06-14 14:30:24 -0600

    Update texture.h

--- ell_SPMM/texture.h
+++ ell_SPMM/texture.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2008-2009 NVIDIA Corporation
+ *  Copyright NVIDIA Corporation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-
-
-
 #pragma once
 
 #include "cuda.h"
